b8c242646caa42991713b32702ef897f1875387b
leoliuyt <leoliuyt@sina.cn>
2018-04-08 14:30:02 +0800

    提交

--- DMRuntime/DMRuntime/ViewController.m
+++ DMRuntime/DMRuntime/ViewController.m
@@ -69,6 +69,16 @@ __weak Person *p_weak_copy   = nil;
             default:
                 break;
         }
+    } else if (self.indexPath.section == 1) {
+        
+    } else if (self.indexPath.section == 2){
+        [self classAssociatedOption];
+    } else if (self.indexPath.section == 3){
+        if(self.indexPath.row == 0){
+            [self dynamicCreateClass];
+        } else {
+            [self dynamicInstance];
+        }
     }
     
     self.associatedObject_assign = [NSString stringWithFormat:@"leoliu1"];
@@ -242,6 +252,102 @@ __weak Person *p_weak_copy   = nil;
     NSString *name = NSStringFromSelector(sel);
     NSLog(@"viewCOntroller = %p == %p ===%@",sel,&name,name);
     [self.father selectorname];
+}
+
+- (void)classAssociatedOption
+{
+    NSLog(@"类相关操作===========\n");
+    NSMutableString *str = [NSMutableString stringWithString:@""];
+    //获取类名
+    const char *charName = class_getName(self.ps.class);
+    NSString *name = [NSString stringWithCString:charName encoding:NSUTF8StringEncoding];
+    [str appendFormat:@"获取类名:%@\n",name];
+    NSLog(@"=================\n");
+    //获取父类
+    const char *superCharName = class_getName(class_getSuperclass(self.ps.class));
+    NSString *superName = [NSString stringWithCString:superCharName encoding:NSUTF8StringEncoding];
+    NSLog(@"获取父类类名:%@",superName);
+    [str appendFormat:@"获取父类类名:%@\n",superName];
+    NSLog(@"=================\n");
+    
+    //是否是元类
+    BOOL isMeta = class_isMetaClass(self.ps.class);
+    [str appendFormat:@"%@是否是元类:%@\n",name,isMeta?@"是":@"否"];
+    NSLog(@"=================\n");
+    
+    //获取元类（objc_方法)
+    Class metaClass = objc_getMetaClass(charName);
+    const char *charMetaClassName = class_getName(metaClass);
+    NSString *metaName = [NSString stringWithCString:charMetaClassName encoding:NSUTF8StringEncoding];
+    [str appendFormat:@"%@的元类名:%@;是否是元类:%@\n",name,metaName,class_isMetaClass(metaClass)?@"是":@"否"];
+    NSLog(@"=================\n");
+    
+    //变量实例大小
+    [str appendFormat:@"%@的变量实例大小:%zu\n",name,class_getInstanceSize(self.ps.class)];
+    NSLog(@"=================\n");
+    
+    //获取指定名称的实例
+    Ivar ivar = class_getInstanceVariable(self.ps.class, "_name");
+    if (ivar != NULL) {
+        [str appendFormat:@"获取%@类中指定_name的实例:%s\n",name,ivar_getName(ivar)];
+    }
+    NSLog(@"=================\n");
+    // 协议
+    unsigned outCount;
+    Protocol * __unsafe_unretained * protocols = class_copyProtocolList(self.ps.class, &outCount);
+    Protocol * protocol;
+    for (int i = 0; i < outCount; i++) {
+        protocol = protocols[i];
+         [str appendFormat:@"获取%@类中遵循的协议名%s\n",name,protocol_getName(protocol)];
+    }
+    BOOL isConforms = class_conformsToProtocol(self.ps.class, protocol);
+    [str appendFormat:@"获取%@类是否遵循了%s协议:%@\n",name,protocol_getName(protocol),isConforms ? @"是":@"否"];
+    NSLog(@"=================\n");
+    self.textView.text = [str copy];
+    self.textView.editable = YES;
+}
+
+- (void)dynamicCreateClass
+{
+    //创建一个继承自self.ps的类SubClass
+    Class subClass = objc_allocateClassPair(self.ps.class, "SubClass", 0);
+    void(^block)(void) = ^(){
+        NSLog(@"block 调用");
+    };
+    
+    IMP imp_subMethod = imp_implementationWithBlock(block);
+    //向SubClass中添加方法
+    class_addMethod(subClass, @selector(addMethod), imp_subMethod, "v@:");
+    //添加实例变量
+    class_addIvar(subClass, "_ivar1", sizeof(NSString *), log(sizeof(NSString *)), @encode(NSString *));
+    
+    //添加属性
+    objc_property_attribute_t type = {"T", "@\"NSString\""};
+    objc_property_attribute_t ownership = { "C", "" };
+    objc_property_attribute_t backingivar = { "V", "_ivar1"};
+    objc_property_attribute_t attrs[] = {type, ownership, backingivar};
+    class_addProperty(subClass, "property2", attrs, 3);
+    
+    //注册
+    objc_registerClassPair(subClass);
+    
+    //创建类的实例
+    id instance = [[subClass alloc] init];
+    [instance performSelector:@selector(addMethod)];
+    [instance setValue:@"hello world" forKey:@"_ivar1"];
+    
+    NSLog(@"_ivar1======%@===",[instance valueForKey:@"_ivar1"]);
+    
+}
+
+- (void)dynamicInstance
+{
+    id theObject = class_createInstance(NSString.class, sizeof(unsigned));
+    
+    id str1 = [theObject init];
+    NSLog(@"%@", [str1 class]);
+    id str2 = [[NSString alloc] initWithString:@"test"];
+    NSLog(@"%@", [str2 class]);
 }
 
 @end
